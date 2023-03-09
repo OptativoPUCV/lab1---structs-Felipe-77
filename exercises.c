04bcfbd3a52ee55c46fb90c28e2649cf34553c4c
Replit user <>
2023-03-09 16:15:07 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -57,8 +57,11 @@ typedef struct {
 Persona* crearPersona(char nombre[], char rut[], int edad) {
    Persona * p = (Persona *) malloc(sizeof(Persona));
    //asignar valores de entrada a los campos de p
+  strcopy(p->nombre, nombre);
+  strcopy(p->rut, rut);
 
-
+  p->edad = edad;
+  
    return p;
 }
 
