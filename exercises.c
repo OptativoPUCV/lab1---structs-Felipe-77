bd4cc749e238777f310d2d408c24ecf82997cf9d
Replit user <>
2023-03-09 15:56:47 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -24,8 +24,12 @@ Programe la función void arrayMaxMin(int *a, int n, int *max, int *min),
 la cual encuentra el máximo y el mínimo valor del arreglo a y los
 almacena en las variables apuntadas por max y min.
 */
-void arrayMaxMin(int *a, int n, int *max, int *min) {
-    
+void arrayMaxMin(int *array, int n, int *max, int *min) {
+    int max = 0;
+    int min = array[0];
+    for (int q = 0; q < n; q++){
+      if (array[q] )
+    }
 }
 
 
