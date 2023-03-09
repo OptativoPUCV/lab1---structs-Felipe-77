44cc93c84f2cd96fff4c035e878e9cec3f7fc9f7
Replit user <>
2023-03-09 20:14:16 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -79,7 +79,7 @@ typedef struct {
 } Vector;
 
 Vector * crearVector(int n) {
-  Vector * arreglo = (Vector *)malloc(n);
+  Vector * arreglo = (Vector *)malloc(n * sizeof(Vector));
 
   for (int q = 0; q < n; q++){
     arreglo->capacidad = n;
